725a33acb6bcf704c955fa23a720754b60e917e4
wu <wu@.com>
2025-09-07 14:14:52 +0800

    run ciciui/django-db-logger/gpt-4o-9

--- setup.py
+++ setup.py
@@ -1,81 +1,38 @@
-import io
+import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
-version = '1.9.0.dev0'
-
-# Please update tox.ini when modifying dependency version requirements
-install_requires = [
-    # load_pem_private/public_key (>=0.6)
-    # rsa_recover_prime_factors (>=0.8)
-    'cryptography>=0.8',
-    # Connection.set_tlsext_host_name (>=0.13)
-    'PyOpenSSL>=0.13',
-    # For pkg_resources. >=1.0 so pip resolves it to a version cryptography
-    # will tolerate; see #2599:
-    'setuptools==65.5.1',
-]
-
-testing_requires = [
-    'coverage>=4.0',
-    'flake8',
-    'mypy',
-    'pytest-cov',
-    'pytest-flake8>=0.5',
-    'pytest>=2.8.0',
-]
-
-dev_extras = [
-    'pytest',
-    'tox',
-]
-
-docs_extras = [
-    'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
-    'sphinx_rtd_theme',
-]
-
-
-with io.open('README.rst', encoding='UTF-8') as f:
-    long_description = f.read()
-
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='josepy',
-    version=version,
-    description='JOSE protocol implementation in Python',
-    long_description=long_description,
-    url='https://github.com/certbot/josepy',
-    author="Certbot Project",
-    author_email='client-dev@letsencrypt.org',
-    license='Apache License 2.0',
-    python_requires='>=3.6',
+    name='django-db-logger',
+    version='0.1.11',
+    packages=['django_db_logger', 'django_db_logger.migrations'],
+    include_package_data=True,
+    license='MIT License',
+    description='Django logging in database',
+    long_description=README,
+    url='https://github.com/CiCiUi/django-db-logger',
+    author='zhangshine',
+    author_email='zhangshine0125@gmail.com',
+    install_requires=['django>=1.9', 'six'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Security',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    install_requires=install_requires,
-    extras_require={
-        'dev': dev_extras,
-        'docs': docs_extras,
-        'tests': testing_requires,
-    },
-    entry_points={
-        'console_scripts': [
-            'jws = josepy.jws:CLI.run',
-        ],
-    },
 )
